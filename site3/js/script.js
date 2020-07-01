34c9135df3d48ce5b6f661c333fda99f2821fca9
Keniden <54241645+Keniden@users.noreply.github.com>
2020-07-01 22:38:52 +0300

    fixed arrow

--- site3/js/script.js
+++ site3/js/script.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
-		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt=""></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt=""></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt=""></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt=""></button>',
 		responsive: [
 			{
 				breakpoint: 992,
 				settings: {
-					dots: true,
 					arrows: false
 				}
 			}
